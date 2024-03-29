725046206ea35439f4d4e00a221d4e3b6489ef44
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-29 17:15:48 +0300

    changes .sql

--- vk_test.sql
+++ vk_test.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: my_bd
--- Время создания: Мар 27 2024 г., 11:50
--- Версия сервера: 8.2.0
--- Версия PHP: 8.2.14
+-- Время создания: Мар 29 2024 г., 14:08
+-- Версия сервера: 8.3.0
+-- Версия PHP: 8.2.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,10 +39,8 @@ CREATE TABLE `User` (
 --
 
 INSERT INTO `User` (`id`, `email`, `password`, `pass_status`) VALUES
-(25, 'v22la11@mail.ru1', '$2y$10$JpQsi8GOnLRL6cgQwem06u9SKMjc6ybnx9.1NmSiVKwGE6kd9c.Fe', 'good'),
-(26, 'vlad2001@mail.ru', '$2y$10$zk0WMn9BAA0ZrsU17PyKxO6z9ricKSzej9XDgmjFalFi8806Uo7B.', 'perfect'),
-(27, 'vlad2021@mail.ru', '$2y$10$ZPTU1BqPc1otoVgKgtHuduhutXIah565M.N/cBqTb3uT2gs.Fq5sK', 'perfect'),
-(28, 'vlad22021@mail.ru', '$2y$10$reKq68CkfVOmq8jc0K1snOTi7eu.N7Q8aNr0QoAKuJ./JVtZJ8LBe', 'perfect');
+(33, 'your@mail.ru', '$2y$10$FYfqlLvgT.z4ndhLERqEZu9RaJM/XC.rk8GjPhDwboL74yN02mQc2', 'perfect'),
+(34, 'vp@mail.ru', '$2y$10$B1PJnIeocuH//W5cjf34ZO7ztTs770Jv1BRHDo.0w/M2r53PAcaXK', 'good');
 
 --
 -- Индексы сохранённых таблиц
@@ -62,7 +60,7 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT для таблицы `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
